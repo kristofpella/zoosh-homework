66beee1a120a7f111f9d4d13d447ada0e180bb70
kristof <kristofpella@gmail.com>
2020-06-08 17:39:03 +0200

    tidy

--- src/components/MovieListContent.js
+++ src/components/MovieListContent.js
@@ -7,42 +7,34 @@ import {
   ListItemText,
 } from "@material-ui/core";
 
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  movieYear: {
-    textAlign: "right",
-  },
-}));
-
-const MovieListContent = ({ movies, fetchMovieFromWikipedia }) => {
-  const classes = useStyles();
-  return (
-    <List dense className={classes.root}>
-      {movies.map((movie) => {
-        return (
-          <ListItem
-            key={movie.id}
-            button
-            onClick={() => fetchMovieFromWikipedia(movie)}
-          >
-            <ListItemAvatar>
-              <Avatar
-                alt={`${movie.title} Poster`}
-                src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-                    : null
-                }
-              />
-            </ListItemAvatar>
-            <ListItemText primary={movie.title} />
-            <ListItemText className={classes.movieYear} primary={movie.year} />
-          </ListItem>
-        );
-      })}
-    </List>
-  );
-};
+const MovieListContent = ({ movies, fetchMovieFromWikipedia }) => (
+  <List dense>
+    {movies.map((movie) => {
+      return (
+        <ListItem
+          key={movie.id}
+          button
+          onClick={() => fetchMovieFromWikipedia(movie)}
+        >
+          <ListItemAvatar>
+            <Avatar
+              alt={`${movie.title} Poster`}
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : null
+              }
+            />
+          </ListItemAvatar>
+          <ListItemText primary={movie.title} />
+          <ListItemText
+            primary={movie.year}
+            primaryTypographyProps={{ align: "right" }}
+          />
+        </ListItem>
+      );
+    })}
+  </List>
+);
 
 export { MovieListContent };
